100bb0ce41d519283c664884416c2565336beffe
juwon0605 <58173061+juwon0605@users.noreply.github.com>
2020-11-08 00:04:07 +0900

    test component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Component_test from './Component_test';
 
 function App() {
-  return <div><h1>Hello!</h1></div>;
+  return (
+    <div>
+    <h1>Hello!</h1>
+    <Component_test />
+  </div>
+  );
 }
 
 export default App;
---
+++ src/Component_test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Component_test() {
+    return <h3>This is test of component</h3>;
+}
+
+export default Component_test;
